53bbd0cecb9cbe588d5b6d64661820e2c1602a9f
taeyeon0319 <xodus1349@likelion.org>
2022-08-06 14:13:35 +0900

    02-4

--- mysite/config/pybo/views.py
+++ mysite/config/pybo/views.py
@@ -8,6 +8,6 @@ def index(request):
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question = Question.objects.get(id=question_id)
     context = {'question' : question}
     return render(request, 'pybo/question_detail.html', context)
